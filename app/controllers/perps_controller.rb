b55c461cb893b08dca08a3cf34842423fdfa3e76
Keith Funk <Keith.C.Funk@gmail.com>
2021-07-16 11:20:52 -0600

    added more perps.

--- app/controllers/perps_controller.rb
+++ app/controllers/perps_controller.rb
@@ -1,12 +1,17 @@
 class PerpsController < ApplicationController
 
     def index
-        perp = Perp.all 
+        perps = Perp.all 
 
-        render json: perp, include: [
+        render json: perps, include: [
             season: {except: [:created_at, :updated_at]},
             episode: { except: [:updated_at, :created_at, :id, :season_id, :perp_id]}
         ],  except: [:updated_at, :created_at]
     end
 
+    def show
+        perp = Perp.find params[:id]
+
+        render json: perp, except: [:created_at, :updated_at]
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :episodes, only: [:index]
   resources :seasons, only: [:index]
   resources :detectives, only: [:index]
-  resources :perps, only: [:index]
+  resources :perps, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
